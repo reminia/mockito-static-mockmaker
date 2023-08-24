010ee4ff98b156173d14de1d6b86722a739d9a7d
sleefd <sleefd@gmail.com>
2023-08-24 15:45:54 +0800

    first version

---
+++ src/main/java/me/yceel/mockito/CustomCodeGen.java
@@ -0,0 +1,84 @@
+package me.yceel.mockito;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.asm.AsmVisitorWrapper;
+import net.bytebuddy.dynamic.ClassFileLocator;
+import net.bytebuddy.dynamic.scaffold.MethodGraph;
+import net.bytebuddy.dynamic.scaffold.TypeValidation;
+import net.bytebuddy.implementation.Implementation;
+import net.bytebuddy.implementation.bytecode.assign.Assigner;
+import org.mockito.internal.creation.bytebuddy.MockMethodInterceptor;
+import org.mockito.internal.debugging.LocationImpl;
+import org.mockito.internal.invocation.RealMethod;
+import org.mockito.invocation.MockHandler;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
+import java.lang.instrument.UnmodifiableClassException;
+import java.lang.reflect.Method;
+import java.security.ProtectionDomain;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Callable;
+
+import static net.bytebuddy.matcher.ElementMatchers.*;
+import static org.mockito.internal.invocation.DefaultInvocationFactory.createInvocation;
+
+public class CustomCodeGen implements ClassFileTransformer {
+    private final Instrumentation instrument;
+    private final ByteBuddy byteBuddy;
+    private final Set<Class<?>> mocked = new HashSet<>();
+
+    public CustomCodeGen(Instrumentation instrument) {
+        this.instrument = instrument;
+        this.byteBuddy = new ByteBuddy().with(TypeValidation.DISABLED).with(Implementation.Context.Disabled.Factory.INSTANCE).with(MethodGraph.Compiler.ForDeclaredMethods.INSTANCE).ignore(isSynthetic().and(not(isConstructor())).or(isDefaultFinalizer()));
+        instrument.addTransformer(this, true);
+    }
+
+    public void mockStaticClass(Class<?> type) {
+        if (!mocked.contains(type)) {
+            mocked.add(type);
+            try {
+                instrument.retransformClasses(type);
+            } catch (UnmodifiableClassException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+        if (classBeingRedefined == null) {
+            return null;
+        }
+        return byteBuddy.redefine(classBeingRedefined, ClassFileLocator.Simple.of(classBeingRedefined.getName(), classfileBuffer)).visit(new AsmVisitorWrapper.ForDeclaredMethods().method(isStatic(), Advice.to(StaticMethodAdvice.class))).make().getBytes();
+    }
+
+    private static class StaticMethodAdvice {
+        @Advice.OnMethodEnter(skipOn = Advice.OnNonDefaultValue.class)
+        private static Callable<?> enter(@Advice.Origin Class<?> type, @Advice.Origin Method method, @Advice.AllArguments Object[] arguments) throws Throwable {
+            Map<Class<?>, MockMethodInterceptor> interceptors = CustomStaticMockMaker.getInterceptors();
+            MockMethodInterceptor interceptor = interceptors.get(type);
+            MockHandler mockHandler = interceptor.getMockHandler();
+            Object ret = mockHandler.handle(createInvocation(type, method, arguments,
+                    //todo: fix realmethod
+                    RealMethod.IsIllegal.INSTANCE, mockHandler.getMockSettings(), new LocationImpl()));
+            return new Callable<Object>() {
+                @Override
+                public Object call() throws Exception {
+                    return ret;
+                }
+            };
+        }
+
+        @Advice.OnMethodExit
+        private static void exit(@Advice.Return(readOnly = false, typing = Assigner.Typing.DYNAMIC) Object returned, @Advice.Enter Callable<?> mocked) throws Throwable {
+            if (mocked != null) {
+                returned = mocked.call();
+            }
+        }
+    }
+}
---
+++ src/main/java/me/yceel/mockito/CustomStaticMockMaker.java
@@ -0,0 +1,79 @@
+package me.yceel.mockito;
+
+import net.bytebuddy.agent.ByteBuddyAgent;
+import org.mockito.exceptions.base.MockitoException;
+import org.mockito.internal.creation.bytebuddy.InlineByteBuddyMockMaker;
+import org.mockito.internal.creation.bytebuddy.MockMethodInterceptor;
+import org.mockito.invocation.MockHandler;
+import org.mockito.mock.MockCreationSettings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.internal.util.StringUtil.join;
+
+public class CustomStaticMockMaker extends InlineByteBuddyMockMaker {
+    private static final Map<Class<?>, MockMethodInterceptor> interceptors = new HashMap<>();
+    private final CustomCodeGen codeGen;
+
+    public CustomStaticMockMaker() {
+        this.codeGen = new CustomCodeGen(ByteBuddyAgent.getInstrumentation());
+    }
+
+    public static Map<Class<?>, MockMethodInterceptor> getInterceptors() {
+        return interceptors;
+    }
+
+    @Override
+    public <T> StaticMockControl<T> createStaticMock(Class<T> type, MockCreationSettings<T> settings, MockHandler handler) {
+        codeGen.mockStaticClass(type);
+        return new CustomInlineController<>(type, interceptors, settings, handler);
+    }
+
+    @Override
+    public MockHandler getHandler(Object mock) {
+        if (mock instanceof Class<?>) {
+            return interceptors.get(mock).getMockHandler();
+        } else {
+            return super.getHandler(mock);
+        }
+    }
+
+    private static class CustomInlineController<T> implements StaticMockControl<T> {
+
+        private final Class<T> type;
+
+        private final Map<Class<?>, MockMethodInterceptor> interceptors;
+
+        private final MockCreationSettings<T> settings;
+
+        private final MockHandler handler;
+
+        private CustomInlineController(Class<T> type, Map<Class<?>, MockMethodInterceptor> interceptors, MockCreationSettings<T> settings, MockHandler handler) {
+            this.type = type;
+            this.interceptors = interceptors;
+            this.settings = settings;
+            this.handler = handler;
+        }
+
+        @Override
+        public Class<T> getType() {
+            return type;
+        }
+
+        @Override
+        public void enable() {
+            if (interceptors.putIfAbsent(type, new MockMethodInterceptor(handler, settings)) != null) {
+                throw new MockitoException(join("For " + type.getName() + ", static mocking is already registered in the current thread", "", "To create a new mock, the existing static mock registration must be deregistered"));
+            }
+        }
+
+        @Override
+        public void disable() {
+            if (interceptors.remove(type) == null) {
+                throw new MockitoException(join("Could not deregister " + type.getName() + " as a static mock since it is not currently registered", "", "To register a static mock, use Mockito.mockStatic(" + type.getSimpleName() + ".class)"));
+            }
+        }
+    }
+
+}
